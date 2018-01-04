587a91e508de92a13699f17f6cdfc4b43c2b71d9
jugbeep <patrickbotkins@gmail.com>
2018-01-03 17:30:00 -0700

    try again5

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/api', function api_index(req, res) {
         "pets": "returns an array of my pets with their 'name', 'type' and 'breed'"
       }
     }
-  }
+  
     message: "Welcome to my personal api! Here's what you need to know!",
     documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
     base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
@@ -56,7 +56,7 @@ app.get('/api', function api_index(req, res) {
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
-    ]
+    ]}
   })
 });
 
